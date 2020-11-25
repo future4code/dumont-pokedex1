5205294f1259a3afbca2f23e4a8ebd12f7bf9079
Pablo <pma765@hotmail.com>
2020-11-25 17:37:08 -0300

    detalhes-do-pokemon-estatico

--- pokedex/src/components/PokemonDetailScreenComponents/Images.js
+++ pokedex/src/components/PokemonDetailScreenComponents/Images.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import styled from 'styled-components'
+
+const ImagesContainer = styled.div`
+    display: flex;   
+    flex-direction: column;  
+    align-items: center;  
+    margin-top: 20px;
+    img {
+        margin: 5px;
+        max-width: 100px;
+    }
+`
+
+
 
 const Images = () => {
     return (
-        <div>Esse é o componente de Imagens</div>
+        <ImagesContainer>
+            <img src= 'https://picsum.photos/100' />
+            <img src= 'https://picsum.photos/101' />
+        </ImagesContainer>
     )
 }
 
